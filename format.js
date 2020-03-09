811670a3bc6b3b9a398a21efc9280253ed2f8d9b
Luiz Corte Real <login+luiz@elo7.com>
2020-03-09 16:44:20 -0300

    [better-dev] Add terser and eslint

--- format.js
+++ format.js
@@ -1,35 +1,45 @@
 define('format', [], function() {
+	'use strict';
+
 	var format = function(firstPart) {
 		var integerLength = firstPart.length,
 			rest = integerLength % 3,
 			start = '';
 
-		if(rest) {
-			start = firstPart.substring(0 , rest) + '.';
+		if (rest) {
+			start = firstPart.substring(0, rest) + '.';
 		}
-		return start + firstPart.substring(rest , integerLength).match(/\d{3}/g).join('.');
-	}
+		return start + firstPart.substring(rest, integerLength).match(/\d{3}/g).join('.');
+	};
 
 	return {
-		'currency' : function(floatValue, prefix) {
-			var normalized = isNaN(floatValue)? parseFloat(floatValue.toString().replace(/[^\d\.,-]/g, '').replace(/,/g, '.').replace(/\.(\d{3})/g, '$1')) : floatValue;
-			if(normalized && !isNaN(normalized)) {
+		'currency': function(floatValue, prefix) {
+			var normalized = floatValue;
+			if (isNaN(floatValue)) {
+				normalized = parseFloat(floatValue.toString()
+					.replace(/[^\d.,-]/g, '')
+					.replace(/,/g, '.')
+					.replace(/\.(\d{3})/g, '$1'));
+			}
+
+			if (prefix) {
+				prefix = prefix.trim() + ' ';
+			} else {
+				prefix = '';
+			}
+
+			if (normalized && !isNaN(normalized)) {
 				var numberParts = normalized.toString().replace('-', '').split('.'),
 					integer = numberParts[0],
 					decimal = ',' + numberParts[1] + '0',
-					prefix = prefix || '';
-
-				if(prefix) {
-					prefix = prefix.trim() + ' ';
-				}
-				sign = normalized < 0 ? '-' : '';
+					sign = normalized < 0 ? '-' : '';
 
 				var firstPart = numberParts[0].replace(/,/g, '');
-				if(firstPart.length > 3) {
+				if (firstPart.length > 3) {
 					integer = format(firstPart);
 				}
 
-				if (numberParts.length == 1) {
+				if (numberParts.length === 1) {
 					decimal = ',00';
 				} else if (numberParts[1].length >= 2) {
 					decimal = ',' + numberParts[1].substring(0, 2);
@@ -39,9 +49,9 @@ define('format', [], function() {
 			}
 			return '';
 		},
-		'currencyToNumber' : function(currency) {
-			var normalized = currency.replace(/[^0-9\,]/g, '').replace(',', '.');
+		'currencyToNumber': function(currency) {
+			var normalized = currency.replace(/[^0-9,]/g, '').replace(',', '.');
 			return parseFloat(normalized);
-		}
-	}
+		},
+	};
 });
